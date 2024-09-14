6c94c9e8e13cda2f7d8a4ce9a594576eb1ae33d3
rayhankabir-me <devrayhankabir@gmail.com>
2024-09-15 00:01:58 +0600

    issue fixed

--- resources/views/todo/update.blade.php
+++ resources/views/todo/update.blade.php
@@ -42,7 +42,7 @@
                             </div>
                             <div class="mb-5">
                                 <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Todo Description</label>
-                                <textarea id="description" name="description" id="description" rows="4" class="@error('description') is-invalid @enderror block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{$todo->title}}</textarea>
+                                <textarea id="description" name="description" id="description" rows="4" class="@error('description') is-invalid @enderror block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">{{$todo->description}}</textarea>
                                 @error('description')
                                 <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{ $message }}</p>
                                 @enderror
